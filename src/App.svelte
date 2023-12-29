419d7215c222556677a1dfd255a7d1e5f9c9cb8b
Fariz <nizaralfariziakbar10@gmail.com>
2023-12-30 01:14:13 +0700

    trying to fix vercel analytics

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,6 @@
 <script lang="ts">
   import { Router } from "@roxi/routify";
   import routes from "../.routify/routes.default.js";
-  import { inject } from '@vercel/analytics';
-
-  inject({ mode: import.meta.env.MODE })
-  console.log(`MODE: ${import.meta.env.MODE}`)
 </script>
 
 <Router {routes} />
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import App from './App.svelte'
 import './app.css';
+import { inject } from '@vercel/analytics';
+
+inject({ mode: import.meta.env.MODE })
 
 const app = new App({ target: document.body })
 
