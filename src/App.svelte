2e8e9f02612987fa7febef84ebb5cd4fbb952590
Fariz <nizaralfariziakbar10@gmail.com>
2023-12-30 01:05:08 +0700

    try to install vercel stuff

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
-    import { Router } from '@roxi/routify';
-    import routes from '../.routify/routes.default.js';
+  import { Router } from "@roxi/routify";
+  import routes from "../.routify/routes.default.js";
+  import { inject } from '@vercel/analytics';
+
+  inject({ mode: import.meta.env.MODE })
 </script>
 
 <Router {routes} />
--- src/main.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var App_svelte_1 = require("./App.svelte");
-require("./app.css");
-var app = new App_svelte_1.default({ target: document.body });
-exports.default = app;
